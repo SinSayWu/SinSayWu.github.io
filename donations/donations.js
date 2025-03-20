85d15e38af458a0b1b08726657cc90f1914752a2
Yuhang Wu <yuhangwu2009@gmail.com>
2025-03-20 12:06:57 -0400

    update all of the css and html stuff

--- donations/donations.js
+++ donations/donations.js
@@ -1,20 +1,34 @@
-function updateAmount(amount) {
-    document.getElementById('amount').innerHTML = amount;
+var playing = false;
+
+function play() {
+    const music = document.getElementById("bg-music");
+    const playlist = ["../images/secret_files/irisu_01.mp3", "../images/secret_files/irisu_02.mp3", "../images/secret_files/irisu_03.mp3", "../images/secret_files/irisu_04.mp3", "../images/secret_files/irisu_05.mp3", "../images/secret_files/irisu_06.mp3", ]
+    music.volume = 0.2;
+    music.src = playlist[Math.floor(Math.random() * playlist.length)];
+
+    if (playing) {
+        music.pause();
+        document.getElementById("speaker").src = "../images/mute.png"
+    } else {
+        music.play();
+        document.getElementById("speaker").src = "../images/speaker.png"
+    }
+    playing = !playing;
 }
 
-function addAmount(amount) {
-    amount += parseInt(document.getElementById('amount').innerHTML);
-    db.ref("amountDonated/").set({
-        amountDonated: amount,
-    }).then(() => {
-        db.ref("amountDonated/").once("value").then(function (amount) {
-            document.getElementById('amount').innerHTML = amount.val().amountDonated;
-        })
-    })
+function check() {
+    username = localStorage.getItem("username")
+    if (username == "jesus" || username == "hbrfan") {
+        document.getElementById("irisu").style.display = "block";
+    }
 }
 
-
-
-db.ref("amountDonated/").on("value", (amount) => {
-    document.getElementById('amount').innerHTML = amount.val().amountDonated;
-})
+window.onload = function() {
+    const music = document.getElementById("bg-music");
+    const playlist = ["../images/secret_files/irisu_01.mp3", "../images/secret_files/irisu_02.mp3", "../images/secret_files/irisu_03.mp3", "../images/secret_files/irisu_04.mp3", "../images/secret_files/irisu_05.mp3", "../images/secret_files/irisu_06.mp3", ]
+    music.addEventListener("ended", function () {
+        music.src = playlist[Math.floor(Math.random() * playlist.length)];
+        music.play();
+    });
+    check();
+}
