343f9c6ab83542fb25d54987a485ae982dd4d3f7
YojouhanSeikatsu <rikusaito64@gmail.com>
2025-03-22 16:13:37 -0400

    added updating leaderboard and notifications

--- donations/donations.js
+++ donations/donations.js
@@ -16,18 +16,83 @@ function play() {
     playing = !playing;
 }
 
-function check() {
-    username = localStorage.getItem("username")
-    if (username == "jesus" || username == "hbrfan" || true) {
-        document.getElementById("irisu").style.display = "block";
-    }
-}
-
 function addAmount(amount) {
     amount += parseInt(document.getElementById('money').innerHTML);
-    alert(amount);
     db.ref(`users/${getUsername()}`).update({
         money: amount,
+    })
+}
+
+function loadLeaderboard() {
+    var leaderboard = document.getElementById('leaderboard');
+
+    db.ref("users/").orderByChild("money").on("value", (object) => {
+        leaderboard.innerHTML = "";
+
+        users = [];
+
+        object.forEach((object_child) => {
+            users.push(object_child.val());
+        })
+
+        users.sort((a, b) => {
+            if (a.timestamp == null) return 1;
+            if (b.timestamp == null) return -1; 
+            return a.timestamp - b.timestamp;
+        });
+
+        users.forEach(function(username) {
+            var leader = document.createElement("div");
+            leader.setAttribute("class", "leader");
+            
+
+            var usernameElement = document.createElement("div");
+            usernameElement.setAttribute("class", "leader-header");
+            usernameElement.innerHTML = username.username + ": ";
+            
+            
+            var usernameAmount = document.createElement("span");
+            usernameAmount.setAttribute("id", "leaderNumber");
+            usernameAmount.innerHTML = username.money || 0;
+            
+            var usernameImage = document.createElement("img");
+            usernameImage.src = "../images/money.png";
+            
+            usernameElement.appendChild(usernameAmount);
+            usernameElement.appendChild(usernameImage);
+
+            
+            var contentElement = document.createElement("div");
+            contentElement.setAttribute("class", "leader-content");
+            contentElement.innerHTML = "testing";
+
+            leader.appendChild(usernameElement);
+            leader.appendChild(contentElement);
+
+            leaderboard.appendChild(leader);
+        })
+    })
+}
+
+function loadNotifications() {
+    var notifications = document.getElementById("notifications");
+
+    db.ref("other/clickernotifications/").on("value", (object) => {
+        notifications.innerHTML = "";
+        
+        let notifs = [];
+
+        object.forEach((object_child) => {
+            notifs.push(object_child.val());
+        })
+
+        notifs.forEach(function(notif) {
+            var notifElement = document.createElement("div");
+            notifElement.setAttribute("class", "notification");
+            notifElement.innerHTML = notif;
+
+            notifications.appendChild(notifElement);
+        })
     })
 }
 
@@ -38,7 +103,6 @@ window.onload = function() {
         music.src = playlist[Math.floor(Math.random() * playlist.length)];
         music.play();
     });
-    check();
     if (getUsername() == null) {
         document.body.innerHTML = "<h1>Please Log in through Pebble because im too lazy to add the feature here</h1><button onclick='../pebble/pebble.js'>Pebble</button>";
         return;
@@ -47,4 +111,7 @@ window.onload = function() {
     db.ref(`users/${getUsername()}`).on("value", (amount) => {
         document.getElementById('money').innerHTML = amount.val().money;
     })
+
+    loadLeaderboard();
+    loadNotifications();
 }
